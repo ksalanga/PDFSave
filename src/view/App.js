5cf3fdbca259a12aaaaa2bfa903adc0acac68c2f
Kenneth Salanga <kennethsalanga@gmail.com>
2022-08-15 19:41:04 -0400

    header and content skeleton

---
+++ src/view/App.js
@@ -0,0 +1,24 @@
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-header-item">Hi</div>
+        <div className="App-header-item">Hi</div>
+        <div className="App-header-item">Hi</div>
+      </header>
+      <div className="App-content">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            rel="noopener noreferrer"
+          >
+            Content
+          </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
