bdd494ba6be3f7704fa1969813535d97b5484ab9
Kenneth Salanga <kennethsalanga@gmail.com>
2022-08-16 17:39:54 -0400

    Modularizing PDFItem Components Separate PDFItemBar and Icons for resuse

--- src/view/components/PDFItem.js
+++ src/view/components/PDFItem.js
@@ -1,15 +1,31 @@
+export function PDFItemBar(props){
+    return (
+        <div className="pdf-item-bar">
+            {props.text ? props.text : "Book"}
+            <div className="pdf-item-bar-right">
+                <img src="/images/down-arrow.png" height="24px" width="24px" alt="down-arrow" />
+            </div>
+        </div>
+    )
+}
+
+
+export function Icon(props) {
+    return (
+        <div>
+            <img src={`/images/${props.imgFilename}`} height={props.height ? props.height : "24px"} width={props.width ? props.width : "24px"}></img>
+        </div>
+    )
+}
+
+
 function PDFItem(props) {
     return (
         <div className="pdf-item">
-            <div className="pdf-item-bar">
-                Book
-                <div className="pdf-item-bar-right">
-                    V
-                </div>
-            </div>
-            <img src="/images/book.png" height={"30px"} width={"30px"}></img>
-            <img src="/images/editing.png" height={"24px"} width={"25px"}></img>
-            <img src="/images/trash.png" height={"24px"} width={"25px"}></img>
+            <PDFItemBar />
+            <Icon imgFilename="book.png"/>
+            <Icon imgFilename="editing.png" height="20px" width="20px"/>
+            <Icon imgFilename="trash.png" height="20px" width="20px"/>
         </div>
     )
 }
