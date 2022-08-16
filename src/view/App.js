7605f63bd0c5decec57b98b468b079c176cff3c5
Kenneth Salanga <kennethsalanga@gmail.com>
2022-08-16 12:59:11 -0400

    changing header and giving content PDFView child

--- src/view/App.js
+++ src/view/App.js
@@ -1,21 +1,17 @@
 import './css/App.css';
 
+import PDFView from './components/PDFView';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="App-header-item">Hi</div>
-        <div className="App-header-item">Hi</div>
-        <div className="App-header-item">Hi</div>
+        <div className="App-header-item"></div>
+        <div className="App-header-item">PDF Save</div>
+        <div className="App-header-item"></div>
       </header>
       <div className="App-content">
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            rel="noopener noreferrer"
-          >
-            Content
-          </a>
+          <PDFView />
       </div>
     </div>
   );
