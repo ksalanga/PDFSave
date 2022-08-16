45b62b00fbbecd167f7c3466cc98c0f4956a54cb
Kenneth Salanga <kennethsalanga@gmail.com>
2022-08-16 13:02:35 -0400

    Initial PDFView Component initial structure and css and loads listview child.

---
+++ src/view/components/PDFView.js
@@ -0,0 +1,11 @@
+import ListView from "./ListView";
+
+function PDFView() {
+    return (
+        <div className="pdf-view">
+            <ListView />
+        </div>
+    )
+}
+
+export default PDFView;
