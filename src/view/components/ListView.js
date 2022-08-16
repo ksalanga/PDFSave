573d2ef9c3253c77d40cc5eabf0a80661b6e0040
Kenneth Salanga <kennethsalanga@gmail.com>
2022-08-16 12:59:54 -0400

    Initial ListView returns list of PDF Items

---
+++ src/view/components/ListView.js
@@ -0,0 +1,14 @@
+import PDFItem from "./PDFItem";
+
+function ListView() {
+    return (
+        <div className="list-view">
+        {/* Returns multiple pdf item components */}
+            <PDFItem />
+            <PDFItem />
+            <PDFItem />
+        </div>
+    )
+}
+
+export default ListView;
