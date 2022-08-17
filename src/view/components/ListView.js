8c265c04765c3f4665084a0721c1fa8797a28c28
Kenneth Salanga <kennethsalanga@gmail.com>
2022-08-17 15:51:50 -0400

    Dynamic ListView: Based on the LV's List and Open property, ListView will return all PDF Items if the Open property is False, and it will return a list with the single, selected PDF Item if the Open property is True.

--- src/view/components/ListView.js
+++ src/view/components/ListView.js
@@ -1,10 +1,21 @@
 import PDFItem from "./PDFItem";
 
-function ListView() {
+function ListView(props) {
+
     return (
         <div className="list-view">
-        {/* Returns multiple pdf item components */}
-            <PDFItem open={true}/>
+            {
+                props.open &&
+                props.list.filter(item => item.id === props.selection).map(item => {
+                    return <PDFItem key={item.id} open={props.open} onSelect={props.onSelect} onPDFChange={props.onPDFChange} {...item} />
+                })
+            }
+            {
+                !props.open &&
+                props.list.map(item => {
+                    return <PDFItem key={item.id} open={props.open} onSelect={props.onSelect} onPDFChange={props.onPDFChange} {...item} />
+                })
+            }
         </div>
     )
 }
