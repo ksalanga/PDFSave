29b96c643de66fcb2e47195f5d16cc6e32cc93e5
Kenneth Salanga <kennethsalanga@gmail.com>
2022-08-23 19:08:04 -0400

    User object store access finished

--- src/model/User.js
+++ src/model/User.js
@@ -1,3 +1,5 @@
+import { openDB, PDFSaveDB } from './DB'
+
 export const dummyUsers = [
     {
         name: 'CoolPerson',
@@ -5,3 +7,34 @@ export const dummyUsers = [
         email: 'email@email.com'
     }
 ]
+
+// get user
+export async function get(key) {
+    try {
+        const db = await openDB()
+        return await db.get(PDFSaveDB.userStore, key)    
+    } catch (error) {
+        console.log(`Something went wrong getting User ${key}`)
+    }
+}
+
+// update user
+export async function update(key) {
+    try {
+        const db = await openDB()
+        const userStore = db.transaction(PDFSaveDB.userStore, 'readwrite').store
+        const user = await userStore.get(key)
+    
+        const valueKeys = Object.keys(values)
+    
+        valueKeys.forEach((key) => {
+            user[key] = values[key]
+        })
+
+        await userStore.put(user, key)
+
+        console.log('successfully updated user', key)
+    } catch (error) {
+        console.log("Something went wrong updating user", error)
+    }
+}
