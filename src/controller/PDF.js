3575f0fcc90db409afb3d26c70873a595cffce49
Kenneth Salanga <kennethsalanga@gmail.com>
2022-08-29 19:35:13 -0400

    PDF Controller BrowserView openTab documentation: - Because of the nature of how service workers, content scripts, and popup apps message each other in the chrome environment, I'm not sure how viable a PDF Controller for this specific case of Browser View <-> App View communication will be anymore. Since each service worker can just talk directly to the app via messaging, I'm not sure where we'll shoehorn in the controller. Will still be useful with the App and Model interaction though.

--- src/controller/PDF.js
+++ src/controller/PDF.js
@@ -4,6 +4,10 @@
  * Communicates between different Views:
  * - Input from one view typically outputs into the other view
  * 
+ * How will view to view work with Chrome extensions?:
+ * - Message passing:
+ * Browser View (Background Script) <------- Message Passing (Controller) -------> App (React.js)
+ * 
  * Communicates between Model and View:
  * - Inputs ALWAYS start from the View level
  * - Input sometimes comes from view into the model, and output back out to the view.
@@ -20,6 +24,26 @@
  * - Open New PDF Tab in Browser View -> Select PDF Item in App View
  * - Open Existing PDF Tab -> Select PDF Item
  */
+export function openTab()
+{
+    /* eslint-disable no-undef */
+    
+    // Listen to the current url:
+    // view ./src/chrome/backgroundTab.js
+
+    // if the current url is a pdf extension (ends in .pdf):
+
+        // search filepath in PDF Store
+
+        // If filepath exists:
+            // Send a message: select existing item in App View
+
+        // If filepath doesn't exist:
+            // put new filepath into PDF Store
+            // Send a message: select existing item in App View
+
+    /* eslint-disable no-undef */
+}
 
 /**
  * Browser View Input to Model Input Function(s):
