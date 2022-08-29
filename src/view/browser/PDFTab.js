81648c4ab727c9033549441163b4f1615288a039
Kenneth Salanga <kennethsalanga@gmail.com>
2022-08-29 19:24:36 -0400

    Background Tab Service Worker: BACKGROUNDTAB.JS SERVICE WORKER IS A TEST FUNCTION. IT WILL MOST LIKELY BE MOVED TO ./src/view/browser/PDFTab.js
    The sole purpose of the backgroundTab.js module is for a service worker to detect if a tab is a url with a .pdf extension, then update the url to the specified #page url query string.

--- src/view/browser/PDFTab.js
+++ src/view/browser/PDFTab.js
@@ -1,4 +1,6 @@
 /**
- * Content Script for accessing DOM of any tab with a PDF Extension
- * Actively checking any open tabs in our Chrome Extension
+ * Service Worker/Background Script for accessing any tab with a PDF Extension
+ * and then changing that PDF extension url to the page left off at.
  */
+
+// https://stackoverflow.com/questions/1891738/how-to-modify-current-url-location-in-chrome-via-extensions
