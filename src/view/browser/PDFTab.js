bf46722fc27f15c90fa7835180b9af98e1ab71d4
Kenneth Salanga <kennethsalanga@gmail.com>
2022-08-29 19:37:37 -0400

    DOM access

--- src/view/browser/PDFTab.js
+++ src/view/browser/PDFTab.js
@@ -3,4 +3,11 @@
  * and then changing that PDF extension url to the page left off at.
  */
 
+/**
+ * IF we can access the DOM with service workers, our PDFTab Browser View will stricty use 
+ * that so that we don't have to use content script file access permissions.
+ * 
+ * If accessing the DOM is only available through content scripts though. We just have to deal with it.
+ */
+
 // https://stackoverflow.com/questions/1891738/how-to-modify-current-url-location-in-chrome-via-extensions
