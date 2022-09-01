7bb2c0c2bd6ac8449545bdec45fe99f8912c32e1
Kenneth Salanga <kennethsalanga@gmail.com>
2022-09-01 19:33:34 -0400

    Commands Listener in Background Script: Current Key is CTRL+SHIFT+Y but might change

--- public/background/pdfTabs.js
+++ public/background/pdfTabs.js
@@ -5,13 +5,39 @@
 // - Context Menu 
 // - Accessing Current URLs
 
+console.log("Right here...")
+
 chrome.commands.onCommand.addListener((command) => {
-  console.log(`Command "${command}" triggered`);
+    console.log("Hello?")
+    switch(command)
+    {
+        case "save-at-page":
+            // Grab Active URL:
+            console.log("Saving at page")
+            const queryInfo = {active: true, lastFocusedWindow: true};
+            chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+                const url = tabs[0].url;
+
+                console.log(url)
+
+                // // If url is valid (ends in .pdf):
+                if (url.endsWith('.pdf'))
+                {
+                    chrome.runtime.sendMessage({status: "open"}, (response) =>
+                    {
+                        console.log("Got response: ", response)
+                    })
+                }
+            });
+            break
+        default:
+            console.log("Invalid Command")
+    }
 });
 
 chrome.tabs.onUpdated.addListener((tabid, changeinfo, tab) =>
 {
-
+    console.log("WHYT DONT COMMANDS WOKR???")
 })
 
 /* eslint-disable no-undef */
