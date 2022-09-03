4e54bec2c0d6d24b6f58db431c0b32505e1e58df
Kenneth Salanga <kennethsalanga@gmail.com>
2022-09-03 11:47:32 -0400

    Window OnExit Routine: For all injected content scripts, whenever a user leaves, the extension asks if they want to leave the site.

--- public/js/content/pdfViewer.js
+++ public/js/content/pdfViewer.js
@@ -5,6 +5,13 @@
 // have to revert BACK to just prepending html as a variable:
 // https://wtools.io/html-to-javascript-converter
 
+window.onbeforeunload = confirmExit;
+
+function confirmExit(e) {
+    $("#exampleModalCenter").modal('show');
+    return "This is the return message. Hopefullyh it is a custom message?"
+}
+
 fetch(chrome.runtime.getURL('/templates/dialog.html')).then(r => r.text()).then(html => {
     $("body").prepend(html)
 });
