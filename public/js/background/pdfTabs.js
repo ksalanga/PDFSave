ad208493d77b6ad52aa944c19808b58bcd09ee7b
Kenneth Salanga <kennethsalanga@gmail.com>
2022-09-04 12:49:30 -0400

    Offline Alert Element: For file:/// schemes, Web Accessible Resources like images can't be loaded. So for file schemes, we add an alertOffline template without an img src. If it's any other scheme, we add the alert template with the PDF logo attached.

--- public/js/background/pdfTabs.js
+++ public/js/background/pdfTabs.js
@@ -31,6 +31,46 @@ chrome.commands.onCommand.addListener((command) => {
 });
 
 
+/**
+ * 
+ * @param url (string) - chrome runtime url of .html file 
+ * @param resource (string) - the type of HTML resource you want to message over
+ * and want the listener to receive. If you don't know how to name the resource, just make this string
+ * the name of the html file you're sending.
+ * ex: modal, alert.
+ */
+function loadHTML(url, resource)
+{
+    fetch(url)
+    .then(r => r.text())
+    .then(htmlString => {
+        const message =
+        {
+            message: "load",
+            resource: resource,
+            data: htmlString
+        }
+
+        chrome.tabs.query( {active: true, currentWindow: true}, (tabs) =>
+        {
+            chrome.tabs.sendMessage(tabs[0].id, message, (response) =>
+            {
+                if (chrome.runtime.lastError)
+                {
+                    console.log(`${message.resource} Load Message Error: `, chrome.runtime.lastError)
+                    return
+                }
+
+                console.log(response.message);
+            })
+        })
+    })
+    .catch((err) =>
+    {
+        console.log(`Fetching ${message.resource} Resource Error:`, err)
+    })
+}
+
 // Load and fetch Resources:
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>
 {
@@ -39,64 +79,53 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>
     {
         // save prompt (modal.html):
         const modalURL = chrome.runtime.getURL('/templates/modal.html')
-        fetch(modalURL)
-        .then(r => r.text())
-        .then(htmlString => {
-            const message =
-            {
-                message: "load",
-                resource: "modal",
-                data: htmlString
-            }
-            chrome.tabs.query( {active: true, currentWindow: true}, (tabs) =>
-            {
-                chrome.tabs.sendMessage(tabs[0].id, message, (response) =>
-                {
-                    if (chrome.runtime.lastError)
-                    {
-                        console.log("Modal Load Message Error: ", chrome.runtime.lastError)
-                        return
-                    }
+        loadHTML(modalURL, "modal")
 
-                    console.log(response.message);
+        if (tab.url.startsWith("file"))
+        {
+            const alertOfflineURL = chrome.runtime.getURL('/templates/alertOffline.html')
+            loadHTML(alertOfflineURL, "alert")
+        }
+        else
+        {
+            // alert (alert.html) + pdf logo
+            const alertURL = chrome.runtime.getURL('/templates/alert.html')
+            fetch(alertURL)
+            .then(r => r.text())
+            .then((html) => {
+                if (alertURL.startsWith("file"))
+                {
+                    return html.replace('src=""', 'src="https://www.clipartmax.com/png/middle/134-1345050_letter-p-free-clipart-letter-p.png"')
+                }
+                return html.replace('src=""', `src="${chrome.runtime.getURL('logo192.png')}"`)
+            })
+            .then(htmlString => {
+                const message =
+                {
+                    message: "load",
+                    resource: "alert",
+                    data: htmlString
+                }
+                chrome.tabs.query( {active: true, currentWindow: true}, (tabs) =>
+                {
+                    chrome.tabs.sendMessage(tabs[0].id, message, (response) =>
+                    {
+                        if (chrome.runtime.lastError)
+                        {
+                            console.log("Alert Load Message Error: ", chrome.runtime.lastError)
+                            return
+                        }
+    
+                        console.log(response.message);
+                    })
                 })
             })
-        })
-        .catch((err) =>
-        {
-            console.log("Fetching Modal Resource Error:", err)
-        })
-
-        // alert (alert.html) + pdf logo
-        const alertURL = chrome.runtime.getURL('/templates/alert.html')
-        fetch(alertURL)
-        .then(r => r.text())
-        .then(html => html.replace('src=""', `src="${chrome.runtime.getURL('logo192.png')}"`))
-        .then(htmlString => {
-            const message =
+            .catch((err) =>
             {
-                message: "load",
-                resource: "alert",
-                data: htmlString
-            }
-            chrome.tabs.query( {active: true, currentWindow: true}, (tabs) =>
-            {
-                chrome.tabs.sendMessage(tabs[0].id, message, (response) =>
-                {
-                    if (chrome.runtime.lastError)
-                    {
-                        console.log("Alert Load Message Error: ", chrome.runtime.lastError)
-                        return
-                    }
-
-                    console.log(response.message);
-                })
+                console.log("Fetching Alert Resource Error:", err)
             })
-        })
-        .catch((err) =>
-        {
-            console.log("Fetching Alert Resource Error:", err)
-        })
+        }
+
     }
 })
     // save alert (alert.html):
