0090f5bce020ca4b2445663ea24837766fae37f2
Kenneth Salanga <kennethsalanga@gmail.com>
2022-09-03 20:32:46 -0400

    Load Web Accessible Resources from B_S to C_S
    Workaround for file:// scheme not being able to access Web Accessible Resource:
    https://stackoverflow.com/questions/69736887/web-accessible-resources-manifest-key-is-ignored-if-respective-resources-are-i
    
    Background script sends:
    - alert html string + logo inside img.src
    - modal html string
    
    Content script consumes messages:
    then loads the respective content depending on the resource type

--- public/js/background/pdfTabs.js
+++ public/js/background/pdfTabs.js
@@ -16,7 +16,7 @@ chrome.commands.onCommand.addListener((command) => {
                 {
                     if (chrome.runtime.lastError)
                     {
-                        console.log("Error: ", chrome.runtime.lastError)
+                        console.log("Command Message Error: ", chrome.runtime.lastError)
                         return
                     }
 
@@ -29,6 +29,77 @@ chrome.commands.onCommand.addListener((command) => {
             console.log("Invalid Command")
     }
 });
+
+
+// Load and fetch Resources:
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>
+{
+    if (changeInfo.status === "complete"
+    && tab.url.endsWith(".pdf"))
+    {
+        // save prompt (modal.html):
+        const modalURL = chrome.runtime.getURL('/templates/modal.html')
+        fetch(modalURL)
+        .then(r => r.text())
+        .then(htmlString => {
+            const message =
+            {
+                message: "load",
+                resource: "modal",
+                data: htmlString
+            }
+            chrome.tabs.query( {active: true, currentWindow: true}, (tabs) =>
+            {
+                chrome.tabs.sendMessage(tabs[0].id, message, (response) =>
+                {
+                    if (chrome.runtime.lastError)
+                    {
+                        console.log("Modal Load Message Error: ", chrome.runtime.lastError)
+                        return
+                    }
+
+                    console.log(response.message);
+                })
+            })
+        })
+        .catch((err) =>
+        {
+            console.log("Fetching Modal Resource Error:", err)
+        })
+
+        // alert (alert.html) + pdf logo
+        const alertURL = chrome.runtime.getURL('/templates/alert.html')
+        fetch(alertURL)
+        .then(r => r.text())
+        .then(html => html.replace('src=""', `src="${chrome.runtime.getURL('logo192.png')}"`))
+        .then(htmlString => {
+            const message =
+            {
+                message: "load",
+                resource: "alert",
+                data: htmlString
+            }
+            chrome.tabs.query( {active: true, currentWindow: true}, (tabs) =>
+            {
+                chrome.tabs.sendMessage(tabs[0].id, message, (response) =>
+                {
+                    if (chrome.runtime.lastError)
+                    {
+                        console.log("Alert Load Message Error: ", chrome.runtime.lastError)
+                        return
+                    }
+
+                    console.log(response.message);
+                })
+            })
+        })
+        .catch((err) =>
+        {
+            console.log("Fetching Alert Resource Error:", err)
+        })
+    }
+})
+    // save alert (alert.html):
 
 chrome.tabs.onUpdated.addListener((tabid, changeinfo, tab) => {})
 
